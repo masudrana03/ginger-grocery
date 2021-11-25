5fee3d4dbf0977bc5f317c814762b86dd9ba8f12
Sohag Hasan <shohaghasan575@gmail.com>
2021-11-26 00:02:48 +0600

    add | delivery boy api added

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -43,6 +43,11 @@ class RouteServiceProvider extends ServiceProvider
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api/v1/api.php'));
 
+            Route::prefix('api/v1/delivery')
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api/v1/delivery.php'));
+
             Route::middleware('web')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
---
+++ routes/api/v1/delivery.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\V1\TestController;
+
+
+/**
+ * 
+ * Api for delivery boy app
+ * 
+ * Base route: /api/v1/delivery
+ */
+
+Route::get('test', [TestController::class, 'test']);
+// Route::post('register', [RegisterController::class, 'register']);
+// Route::post('login', [LoginController::class, 'login']);
+
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('test-auth', [TestController::class, 'testAuth']);
+    // Route::get('logout', [LoginController::class, 'logout']);
+    // Route::get('profile', [ProfileController::class, 'profile']);
+    // Route::get('orders', [OrdersController::class, 'orders']);
+    // Route::get('orders/{order}', [OrdersController::class, 'order']);
+    // Route::post('orders/{order}/complete', [OrdersController::class, 'complete']);
+    // Route::post('orders/{order}/cancel', [OrdersController::class, 'cancel']);
+    // Route::post('orders/{order}/pickup', [OrdersController::class, 'pickup']);
+    // Route::post('orders/{order}/deliver', [OrdersController::class, 'deliver']);
+    // Route::post('orders/{order}/rate', [OrdersController::class, 'rate']);
+});
