9ad28ce22fd6ebd2b3be30a7b98794001d538e04
mohasinhossain <mohasin2911@gmail.com>
2021-12-14 11:17:43 +0600

    Firebase notification integrated

---
+++ app/Notifications/Delivery/NewOrderPlaced.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Notifications\Delivery;
+
+use Illuminate\Bus\Queueable;
+use NotificationChannels\Fcm\FcmMessage;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use NotificationChannels\Fcm\Resources\ApnsConfig;
+use NotificationChannels\Fcm\Resources\AndroidConfig;
+use NotificationChannels\Fcm\Resources\AndroidFcmOptions;
+
+class NewOrderPlaced extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return [FcmChannel::class];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toFcm($notifiable)
+    {
+        return FcmMessage::create()
+            ->setData(['data1' => 'value', 'data2' => 'value2'])
+            ->setNotification(\NotificationChannels\Fcm\Resources\Notification::create()
+                ->setTitle('New Order Placed')
+                ->setBody('A new order has been placed.')
+            );
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
