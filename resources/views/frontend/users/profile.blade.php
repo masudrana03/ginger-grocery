ec7a9ba9109771e6bdcb363244e0e603d3bff4d0
safin777 <sayedislamsafin7770555@gmail.com>
2022-01-27 20:12:12 +0600

    Jquery calender done

--- resources/views/frontend/users/profile.blade.php
+++ resources/views/frontend/users/profile.blade.php
@@ -145,9 +145,6 @@
 
 @endsection
 
-{{-- <link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css"> --}}
- {{-- <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
-<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script> --}}
 @push('script')
 <script>
     $(function() {
