decd228c4c64560fb6cee3b2d0377d40e1108c1f
mohasinhossain <mohasin2911@gmail.com>
2022-01-27 18:54:20 +0600

    order modification for addmin & store | fixed payment status update

--- database/seeders/OrderStatusSeeder.php
+++ database/seeders/OrderStatusSeeder.php
@@ -5,45 +5,46 @@ namespace Database\Seeders;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
-class OrderStatusSeeder extends Seeder {
+class OrderStatusSeeder extends Seeder
+{
     /**
      * Run the database seeds.
      *
      * @return void
      */
-    public function run() {
-        DB::table( 'order_statuses' )->insert( [
+    public function run()
+    {
+        DB::table('order_statuses')->insert([
             'name'       => 'Accepted',
             'created_at' => now(),
-        ] );
-
-        DB::table( 'order_statuses' )->insert( [
+        ]);
+        DB::table('order_statuses')->insert([
+            'name'       => 'Pending',
+            'created_at' => now(),
+        ]);
+        DB::table('order_statuses')->insert([
             'name'       => 'Processing',
             'created_at' => now(),
-        ] );
-        DB::table( 'order_statuses' )->insert( [
+        ]);
+        DB::table('order_statuses')->insert([
             'name'       => 'Picked up',
             'created_at' => now(),
-        ] );
-        DB::table( 'order_statuses' )->insert( [
+        ]);
+        DB::table('order_statuses')->insert([
             'name'       => 'Product On The Way',
             'created_at' => now(),
-        ] );
-        DB::table( 'order_statuses' )->insert( [
+        ]);
+        DB::table('order_statuses')->insert([
             'name'       => 'Delivered',
             'created_at' => now(),
-        ] );
-        DB::table( 'order_statuses' )->insert( [
-            'name'       => 'Delivered',
-            'created_at' => now(),
-        ] );
-        DB::table( 'order_statuses' )->insert( [
+        ]);
+        DB::table('order_statuses')->insert([
             'name'       => 'Canceled',
             'created_at' => now(),
-        ] );
-        DB::table( 'order_statuses' )->insert( [
+        ]);
+        DB::table('order_statuses')->insert([
             'name'       => 'Payment Failed',
             'created_at' => now(),
-        ] );
+        ]);
     }
 }
