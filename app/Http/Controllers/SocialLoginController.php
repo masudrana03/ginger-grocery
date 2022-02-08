a5aaf12d879aee79116eb777aa7f9b49b5aca404
masudrana03 <920mash@gmail.com>
2022-02-08 18:54:21 +0600

    Social media backend working 80% done

---
+++ app/Http/Controllers/SocialLoginController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SocialLoginController extends Controller
+{
+   public function socialIndex()
+   {
+       # code...
+   }
+}
