d94c25c96390070eab93a85af1e88c0acddfbc7a
Masud Rana <63516873+masudrana03@users.noreply.github.com>
2022-03-03 12:12:13 +0600

    Frontend | product details some issue fix.

--- resources/views/frontend/product-details.blade.php
+++ resources/views/frontend/product-details.blade.php
@@ -260,13 +260,17 @@
                                                 <p>{{ $product->description }}</p>
 
                                                 <ul class="product-more-infor mt-30">
-                                                    <li><span>Type Of Packing</span> Bottle</li>
-                                                    <li><span>Color</span> Green, Pink, Powder Blue, Purple</li>
-                                                    <li><span>Quantity Per Case</span> 100ml</li>
-                                                    <li><span>Ethyl Alcohol</span> 70%</li>
-                                                    <li><span>Piece In One</span> Carton</li>
+                                                    <li><span>Attributes</span> {{ $product->attributes ?? 'Not found' }} </li>
+                                                    <li><span>Calories Per Serving</span> {{ $product->calories_per_serving ?? 'Not found'}} </li>
+                                                    <li><span>Fat Calories Per Serving</span> {{ $product->fat_calories_per_serving ?? 'Not found' }} </li>
+                                                    <li><span>Brand</span> {{ $product->brand->name ? $product->brand->name : 'Not found' }} </li>
+                                                    <li><span>Category</span> {{ $product->category->name ?? 'Not found' }} </li>
+                                                    <li><span>Type</span> {{  count($product->types) > 0 ? $product->types->pluck('name')->implode(', ') : 'Not found'  }} </li>
+                                                    <li><span>Unit</span> {{ $product->unit->name  ?? 'Not found' }} </li>
+                                                    <li><span>Nutrition</span> {{ count($product->nutritions) > 0 ? $product->nutritions->pluck('name')->implode(', ') : 'Not found'}} </li>
                                                 </ul>
-                                                <hr class="wp-block-separator is-style-dots" />
+
+                                                {{-- <hr class="wp-block-separator is-style-dots" />
                                                 <p>Laconic overheard dear woodchuck wow this outrageously taut beaver hey
                                                     hello far meadowlark imitatively egregiously hugged that yikes minimally
                                                     unanimous pouted flirtatiously as beaver beheld above forward energetic
@@ -286,20 +290,23 @@
                                                     dalmatian in much less well jeering for the thanks blindly sentimental
                                                     whimpered less across objectively fanciful grimaced wildly some wow and
                                                     rose jeepers outgrew lugubrious luridly irrationally attractively
-                                                    dachshund.</p>
+                                                    dachshund.</p> --}}
+
                                                 <h4 class="mt-30">Suggested Use</h4>
                                                 <ul class="product-more-infor mt-30">
                                                     <li>Refrigeration not necessary.</li>
                                                     <li>Stir before serving</li>
                                                 </ul>
-                                                <h4 class="mt-30">Other Ingredients</h4>
+
+                                                {{-- <h4 class="mt-30">Other Ingredients</h4>
                                                 <ul class="product-more-infor mt-30">
                                                     <li>Organic raw pecans, organic raw cashews.</li>
                                                     <li>This butter was produced using a LTG (Low Temperature Grinding)
                                                         process</li>
                                                     <li>Made in machinery that processes tree nuts but does not process
                                                         peanuts, gluten, dairy or soy</li>
-                                                </ul>
+                                                </ul> --}}
+
                                                 <h4 class="mt-30">Warnings</h4>
                                                 <ul class="product-more-infor mt-30">
                                                     <li>Oil separation occurs naturally. May contain pieces of shell.</li>
