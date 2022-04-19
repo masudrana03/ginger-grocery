a09c746d478a923571a37570e0e9f4ee616f31b3
masudrana03 <920mash@gmail.com>
2022-04-19 14:22:28 +0600

    Backend | working on order status

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -67,6 +67,7 @@ class OrderController extends Controller
         }
 
         $orderStatus = $request->status;
+        logger($orderStatus);
 
         if ($orderStatus) {
             //$orderStatus = OrderStatus::whereName($orderStatus)->firstOrFail();
@@ -96,12 +97,13 @@ class OrderController extends Controller
 
         $data = [];
         $orderStatuses = OrderStatus::all();
+        // logger($orderStatuses);
 
         if (!empty($orders)) {
             foreach ($orders as $order) {
                 $updatePaymentStatus = route('admin.orders.updatePaymentStatus', $order->id);
                 $show                = route('admin.orders.show', $order->id);
-                $edit                = route('admin.orders.edit', $order->id);
+                // $edit                = route('admin.orders.edit', $order->id);
                 $delete              = route('admin.orders.destroy', $order->id);
                 $token               = csrf_token();
                 $status              = '';
@@ -137,12 +139,17 @@ class OrderController extends Controller
                 $nestedData['created_at'] = $order->created_at->format('d-m-Y');
                 $nestedData['actions']    = "
                     <a href='{$show}' title='DETAILS' ><span class='far fa-eye'></span></a>
-                    &emsp;<a href='#' onclick='deleteorder({$order->id})' title='DELETE' ><span class='fas fa-trash'></span></a>
-                    <form id='delete-form-{$order->id}' action='{$delete}' method='POST' style='display: none;'>
-                    <input type='hidden' name='_token' value='{$token}'>
-                    <input type='hidden' name='_method' value='DELETE'>
-                    </form>
+
                     ";
+                    // &emsp;<a href='#' onclick='deleteorder({$order->id})' title='DELETE' ><span class='fas fa-trash'></span></a>
+                    // <form id='delete-form-{$order->id}' action='{$delete}' method='POST' style='display: none;'>
+                    // <input type='hidden' name='_token' value='{$token}'>
+                    // <input type='hidden' name='_method' value='DELETE'>
+                    // </form>
+
+
+
+
                 $data[] = $nestedData;
             }
         }
@@ -200,7 +207,7 @@ class OrderController extends Controller
     public function destroy(Order $order)
     {
         $order->status()->delete();
-        $order->delete();
+        $order->refresh()->delete();
 
         toast('Order successfully deleted', 'success');
 
